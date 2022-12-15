f1f0044f502ab1340e917617ec1afcfe342fa8bb
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2022-12-14 23:06:33 -0300

    Criação da pasta shared, para onde o arquivo de bindings foi movido.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:todo_app_siliconvillage/bindings.dart';
+import 'package:todo_app_siliconvillage/shared/bindings.dart';
 import 'presentation/screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
